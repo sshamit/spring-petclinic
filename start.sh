65d9fbe6b35902e7534a7d587b0472549eda1c53
root <root@virtualserver01.Amit-Kushwaha-s-Account.cloud>
2018-12-06 11:35:22 +0000

    Added script

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+java -jar target/*.jar &      # You send it in background
+MyPID=$!                        # You sign it's PID
+echo $MyPID                     # You print to terminal
+chmod +x stop.sh
+echo "kill -9 $MyPID" > stop.sh  # Write the the command kill pid in MyStop.sh
